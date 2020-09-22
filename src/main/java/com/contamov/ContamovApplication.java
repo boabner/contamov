8ed621f2299cc2e16b8e5035cadb105e9670401c
boabner <boabner@gmail.com>
2020-09-22 06:25:17 -0400

    first commit

---
+++ src/main/java/com/contamov/ContamovApplication.java
@@ -0,0 +1,13 @@
+package com.contamov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContamovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContamovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contamov/ContamovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contamov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContamovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
